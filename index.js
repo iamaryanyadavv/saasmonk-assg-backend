354467bea95ef42c84253bd44fe1a9e3a68e977c
Aryan Yadav <aryanyadav1601@gmail.com>
2024-03-20 16:08:01 +0530

    Added basic endpoints and connected to database

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { ObjectId } from 'mongodb';
+import client from './database.js';
+import cors from 'cors';
+
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(express.json());
+app.use(express.json()); 
+app.use(cors()); // Enable CORS for all routes
 
-app.listen(8800, () => {
-    console.log("Connected!");
+app.post('/movies', async (req, res) => {
+    console.log("Adding Movie")
+  try {
+    const collection = client.db("SaasMonk_Movies").collection("Movies");
+    const post = {
+        name: req.body.name,
+        releaseDate: req.body.releaseDate,
+        totalStars: 0,
+        totalReviews: 0
+    };
+
+    await collection.insertOne(post);
+    return res.json("Movie has been created.");
+    } catch (error) {
+        console.error("Error executing query:", error);
+        return res.status(500).json(error);
+    }
+});
+
+app.get('/movies', async (req, res) => {
+    try {
+        const cat = req.query.cat;
+        const collection = client.db("SaasMonk_Movies").collection("Movies");
+        const query = cat ? { cat } : {};
+        
+            const data = await collection.find(query).toArray();
+            return res.status(200).json(data);
+        } catch (error) {
+            console.error("Error executing query:", error);
+            return res.status(500).json(error);
+        }
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
